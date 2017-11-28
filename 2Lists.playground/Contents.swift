4ed7319dafbf1d8d8f810b5b4455cee75f3cf1a5
Graham Lee <graham.lee@wealthwizards.com>
2017-11-28 14:04:44 +0000

    extend the lists episode

--- 2Lists.playground/Contents.swift
+++ 2Lists.playground/Contents.swift
@@ -27,3 +27,18 @@ let noIntegers: List<Int> = EmptyList()
 
 noIntegers.count()
 noIntegers.at(0)
+
+// as with sets, we can build lists that close over their constructor parameters as instance variables.
+// the easiest way to build a list of arbitrary length is to build one with two members.
+func LinkedList<T>(head: T, tail: List<T>) -> List<T>
+{
+    return List(count: { return 1 + tail.count() },
+                at: {index in return (index == 0) ? head : tail.at(index - 1)})
+}
+
+let oneTwoThree : List<Int> = LinkedList(head: 1, tail: LinkedList(head: 2, tail: LinkedList(head: 3, tail: EmptyList())))
+
+oneTwoThree.count()
+oneTwoThree.at(0)
+oneTwoThree.at(1)
+
